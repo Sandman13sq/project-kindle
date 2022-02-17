f7626da02001c1a6a918f0142b5662377d69e237
CarbyDood <d-iraheta1@hotmail.com>
2022-02-16 21:33:40 -0500

    implemented variable jump, and movement with adjustable momentum

--- project-kindle/Assets/Scenes/CharacterControlsOld.cs
+++ project-kindle/Assets/Scenes/CharacterControlsOld.cs
@@ -7,9 +7,8 @@ using UnityEngine.SceneManagement;
 public class CharacterControlsOld : MonoBehaviour
 {
     //Constants
-    private float moveSpeed = 8;
+    private float maxSpeed = 8;
     private float jumpForce = 4.0f;
-    private float acceleration;
     //Setting up the RigidBody2D object so that we can mess with it in code
     private Rigidbody2D rigBod;
 
@@ -19,10 +18,11 @@ public class CharacterControlsOld : MonoBehaviour
         rigBod = gameObject.GetComponent<Rigidbody2D>();
         //basically your cout for unity, will spit out a message in the console
         Debug.Log("Tatu wuz here!");
+        rigBod.freezeRotation = true;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //call the movement handler every frame
         movementHandler();
@@ -31,6 +31,7 @@ public class CharacterControlsOld : MonoBehaviour
     //movement handler checks if the player is holding down any movement keys or the jump buttons, and makes
     //kindle move accordingly
     void movementHandler() {
+        //********MOVEMENT************
         //Input.GetAxis() gets the value of whatever is being pressed down. "Horizontal" checks for the left and right arrows,
         //A and D, and the left and right stick of a controller. for keyboard controls, slowly builds up to -1 and 1.
         float leftRite = Input.GetAxis("Horizontal"); //left = -1, right = 1
@@ -38,11 +39,13 @@ public class CharacterControlsOld : MonoBehaviour
         //Vectors are basically arrays of floats. access them using moveVect.x, y, z
         //timedelta is the time it takes for 1 frame to process. we use it here to smooth out
         //the movement
-        Vector3 moveVect = new Vector3(leftRite, 0.0f, 0.0f) * Time.deltaTime * moveSpeed;
+        Vector3 moveVect = new Vector3(leftRite, 0.0f, 0.0f) * Time.deltaTime * maxSpeed;
         Vector2 jumpStr = new Vector2(0, jumpForce);
 
         //transform.position gets the current position of the sprite, you can mess with it however you like
         transform.position += moveVect;
+
+        //*********JUMPING*************
 
         //if the z key is being pushed down, and while we are not already in the air, we jump.
         //the second part of this condition is so the player can't double jump.
--- project-kindle/Assets/Scripts/CharacterMovement.cs
+++ project-kindle/Assets/Scripts/CharacterMovement.cs
@@ -3,84 +3,110 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     //constants for movement
-    private float speedCap = 8.0f;
-    private float jumpHeight = 8.0f;
-    private float moveAccel = 0.3f;
-    private float jumpAccel = 1.5f;
+    private float maxSpeed = 8.0f;
+    private float jumpHeight = 2.0f;
+    private float moveAccel = 16.0f;
+    
+    private LayerMask PlatformLayer = 64;
+    //bools for jumping
+    //bool to check if the jump button is being held and if we are in a jump
+    bool jumpIsHeld = false;
+    bool inJump = false;
+    bool isGrounded = false;
 
-    //variable to hold the rigidbody component in case we need to manipulate it
+    //variable to hold the components in case we need to manipulate it
     private Rigidbody2D rigBod;
+    private BoxCollider2D hitbox;
 
     //variable to hold the current movement speed
     private float moveSpeed = 0.0f;
-     private float jumpSpeed = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
         rigBod = gameObject.GetComponent<Rigidbody2D>();
+        hitbox = gameObject.GetComponent<BoxCollider2D>();
         Debug.Log("Tatu wuz here!");
+        rigBod.freezeRotation = true;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+        isGrounded = checkGround();
         //calls movementHandler every frame
         movementHandler();
+
     }
 
     //Handles movement by check if left or right are being pressed or if the jump key is being held down
     void movementHandler(){
         Vector3 moveVect = new Vector3(moveSpeed, 0.0f, 0.0f) * Time.deltaTime;
         
-        //*****MOVING RIGHT********
-        //when right arrow is held down, move in the positive x direction
-        if(Input.GetKey("right")){
-            //we only accelerate until we hit the movement speed cap
-            if(Mathf.Abs(moveSpeed) < speedCap){
-                moveSpeed += moveAccel;
-            }
-            transform.position += moveVect;
+        //*****MOVING********
+        //when right arrow is held down and the speed is less than the maxspeed, accelerate in the positive or negative x direction
+        //respectively
+        if(Input.GetKey("right") && moveSpeed > -maxSpeed){
+            moveSpeed = moveSpeed + (moveAccel * Time.deltaTime);
         }
 
-        //******MOVING LEFT********
-        //when left arrow is held down, move in the negative x direction
-        if(Input.GetKey("left")){
-            //we only accelerate until we hit the movement speed cap
-            if(Mathf.Abs(moveSpeed) < speedCap){
-                moveSpeed -= moveAccel;
-            }
-            transform.position += moveVect;
+        else if(Input.GetKey("left") && moveSpeed < maxSpeed){
+            moveSpeed = moveSpeed - (moveAccel * Time.deltaTime);
         }
 
-        //slow down if we are moving still and have let go of a movement key
-        if (!Input.GetKey("right") && !Input.GetKey("left")){
-            //if the velocity of out sprite is going too far right or left, move it in the opposite direction
-            //moving right still
-            if(moveSpeed > 0){
-                transform.position += moveVect;
-                moveSpeed -= moveAccel;
+        //if neither button is pressed, slow down to zero if we're still movin'
+        else{
+            if(moveSpeed > (moveAccel*Time.deltaTime)){
+                moveSpeed = moveSpeed - (moveAccel * Time.deltaTime);
             }
-
-            //moving left still
-            if(moveSpeed < 0){
-                transform.position += moveVect;
-                moveSpeed += moveAccel;
+            else if(moveSpeed < (-moveAccel*Time.deltaTime)){
+                moveSpeed = moveSpeed + (moveAccel * Time.deltaTime);
             }
-
-            //rounding issues occur with this implementation, if we are moving less than 0.1, then set it to 0
-            if(Mathf.Abs(moveSpeed) < 0.1){
-                moveSpeed = 0.0f;
+            //set movespeed to zero in any other case to prevent weirdness.
+            else{
+                moveSpeed = 0;
             }
         }
+
+        //apply the speed
+        moveVect = new Vector3(moveSpeed * Time.deltaTime, 0.0f, 0.0f);
+        transform.position += moveVect;
 
         //****JUMPING*****
-        Debug.Log("Jumpspeed: " + jumpSpeed);
-        //Check if Z is being pressed, if so, jump!
-        if(Input.GetKeyDown("z") && Mathf.Abs(jumpSpeed) <= 0.01){
-            jumpSpeed = jumpHeight;
-            Vector3 jumpVect = new Vector3(0.0f, jumpSpeed, 0.0f) * Time.deltaTime * jumpHeight;
-            transform.position += jumpVect;
-            jumpSpeed -= jumpAccel;
+        //calculate the jump force
+        float jumpForce = Mathf.Sqrt(2 * Physics2D.gravity.magnitude * jumpHeight);
+
+        //counter jumpForce to push down on object
+        Vector2 counterJumpForce = new Vector2(0.0f, -jumpForce);
+
+        if(Input.GetKeyDown("z")){
+            jumpIsHeld = true;
+
+            if(isGrounded){
+                inJump = true;
+                rigBod.AddForce(Vector2.up * jumpForce * rigBod.mass, ForceMode2D.Impulse);
+            }
         }
+
+        else if(Input.GetKeyUp("z")){
+            jumpIsHeld = false;
+        }
+
+        //apply the counter force while sprite is in the air
+        if(inJump){
+            if(!jumpIsHeld && Vector2.Dot(rigBod.velocity, Vector2.up) > 0){
+                rigBod.AddForce(counterJumpForce * rigBod.mass);
+            }
+        }
+    }
+
+    public bool checkGround(){
+        //basically sees if the box around the sprite is in contact with the floor. Return whatever the results are.
+        float extraHeight = 1.0f;
+
+        //boxsize is shortened a bit to avoid detecting walls
+        Vector3 boxSize = hitbox.bounds.size - new Vector3(0.1f, 0f, 0f);
+        RaycastHit2D hit = Physics2D.BoxCast(hitbox.bounds.center, boxSize, 0f, Vector2.down, extraHeight, PlatformLayer);
+
+        return hit.collider != null;
     }
 }
