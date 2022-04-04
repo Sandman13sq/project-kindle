ac136dd832a8eb040ef1d16329d935bee50a639d
Dreamer <dreamer13sq@gmail.com>
2022-04-03 20:31:06 -0400

    Started on Textbox

---
+++ project-kindle/Assets/__Game/GUI/Textbox/Textbox.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Textbox : MonoBehaviour
+{
+    // ====================================================================
+
+    // Variables
+
+    [SerializeField] private UnityEngine.UI.Image boxsprite;
+    [SerializeField] private RectTransform boxrect;
+
+    // ====================================================================
+
+    // Functions
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
