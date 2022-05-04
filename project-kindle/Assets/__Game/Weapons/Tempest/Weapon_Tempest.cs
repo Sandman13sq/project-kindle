979ea259d72b80bb7a30d3669deb1ad2f5c6cec8
Dreamer <dreamer13sq@gmail.com>
2022-05-04 10:35:11 -0400

    Initialized Tempest Weapon

---
+++ project-kindle/Assets/__Game/Weapons/Tempest/Weapon_Tempest.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon_Tempest : Weapon
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
