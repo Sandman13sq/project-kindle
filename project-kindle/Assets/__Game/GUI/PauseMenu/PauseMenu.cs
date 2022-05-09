0078f2a2c4a61bd2f9377b501099952425d65c70
CarbyDood <d-iraheta1@hotmail.com>
2022-05-09 01:35:07 -0400

    Implemented Weapons, Enemies, and Goals tabs in pause menu. Navigatable with Mouse and keyboard. Noticed that controller support has been busted. TODO

--- project-kindle/Assets/__Game/GUI/PauseMenu/PauseMenu.cs
+++ project-kindle/Assets/__Game/GUI/PauseMenu/PauseMenu.cs
@@ -1,16 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenu : MasterObject
 {
     public static bool isPaused = false;
+    private int currentTab = 1; //0 = Goals, 1 = Weapons, 2 = Enemies
+    [SerializeField] GameObject pauseMenuUI;
+    [SerializeField] GameObject weaponPage;
+    [SerializeField] GameObject weaponPage1;
+    [SerializeField] GameObject weaponPage2;
+    [SerializeField] GameObject goalsPage;
+    [SerializeField] GameObject enemiesPage;
+    [SerializeField] Button weaponsButton;
+    [SerializeField] Button goalsButton;
+    [SerializeField] Button enemiesButton;
+    [SerializeField] UnityEngine.UI.Image[] arrowsprites;
+    private float statestep;
+    private List<float> arrowystart = new List<float>();
 
-    public GameObject pauseMenuUI;
+    void Start() {
+        int len = arrowsprites.Length;
+        for (int i = 0; i < len; i++)
+        {
+            arrowystart.Add(arrowsprites[i].transform.localPosition.y);
+        }
+        currentTab = 1;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("c") && Input.GetKeyDown("return"))
+        if(Input.GetKeyDown("c") || Input.GetKeyDown("return"))
         {
             if(isPaused)
             {
@@ -25,6 +47,87 @@ public class PauseMenu : MasterObject
                 game.GameFlagSet(_GameHeader.GameFlag.pause);
             }
         }
+
+        //Goals Page
+        if(currentTab == 0)
+        {
+            goalsButton.Select();
+        }
+
+        //Weapons Page
+        else if(currentTab == 1)
+        {
+            weaponsButton.Select();
+            if(Input.GetKeyDown("down"))
+            {
+                weaponPage1.SetActive(false);
+                weaponPage2.SetActive(true);
+                statestep = 0.0f;
+            }
+
+            else if(Input.GetKeyDown("up"))
+            {
+                weaponPage2.SetActive(false);
+                weaponPage1.SetActive(true);
+                statestep = 0.0f;
+            }
+        }
+
+        //Enemies Page
+        else if(currentTab == 2)
+        {
+            enemiesButton.Select();
+        }
+
+        //navigate to the correct page
+        if(Input.GetKeyDown("left"))
+        {
+            //From weapons page, go to the goals page
+            if(currentTab == 1)
+            {
+                weaponPage.SetActive(false);
+                goalsPage.SetActive(true);
+                currentTab = 0;
+            }
+
+            //from enemies page, go to weapons page
+            else if(currentTab == 2)
+            {
+                enemiesPage.SetActive(false);
+                weaponPage.SetActive(true);
+                currentTab = 1;
+            }
+        }
+
+        if(Input.GetKeyDown("right"))
+        {
+            //From weapons page, go to enemies page
+            if(currentTab == 1)
+            {
+                weaponPage.SetActive(false);
+                enemiesPage.SetActive(true);
+                currentTab = 2;
+            }
+            //From goals page, go to weapons page
+            if(currentTab == 0)
+            {
+                goalsPage.SetActive(false);
+                weaponPage.SetActive(true);
+                currentTab = 1;
+            }
+        }
+
+        // Arrow bobbing
+        statestep += 0.1f;
+        int index = 0;
+        foreach(UnityEngine.UI.Image arrow in arrowsprites)
+        {
+            arrow.transform.localPosition = new Vector2(
+                arrow.transform.localPosition.x,
+                arrowystart[index] + Mathf.Sin(statestep)*4.0f
+            );
+            index++;
+        }
     }
 
     void Resume()
@@ -32,6 +135,7 @@ public class PauseMenu : MasterObject
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
+        statestep = 0.0f;
     }
 
     void Pause()
@@ -39,5 +143,28 @@ public class PauseMenu : MasterObject
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
+        statestep = 0.0f;
+    }
+
+    //Mouse Controls
+    public void OnClickGoals(){
+        currentTab = 0;
+        weaponPage.SetActive(false);
+        enemiesPage.SetActive(false);
+        goalsPage.SetActive(true);
+    }
+
+    public void OnClickWeapons(){
+        currentTab = 1;
+        weaponPage.SetActive(true);
+        enemiesPage.SetActive(false);
+        goalsPage.SetActive(false);
+    }
+
+    public void OnClickEnemies(){
+        currentTab = 2;
+        weaponPage.SetActive(false);
+        enemiesPage.SetActive(true);
+        goalsPage.SetActive(false);
     }
 }
---
+++ project-kindle/Assets/__Game/GUI/PauseMenu/WeaponMeters.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponMeters : MasterObject
+{
+    [SerializeField] private HUDMeter vectorMeter;
+    [SerializeField] private GameObject vectorLevel; 
+    [SerializeField] private HUDMeter dragonMeter;
+    [SerializeField] private GameObject dragonLevel; 
+    [SerializeField] private HUDMeter tempestMeter;
+    [SerializeField] private GameObject tempestLevel; 
+    [SerializeField] private HUDMeter meteorMeter;
+    [SerializeField] private GameObject meteorLevel;
+
+    private UnityEngine.UI.Text vectorLevelText;
+    private UnityEngine.UI.Text dragonLevelText;
+    private UnityEngine.UI.Text tempestLevelText;
+    private UnityEngine.UI.Text meteorLevelText;
+
+    private PlayerData playerdata;
+    private Weapon[] weapons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerdata = game.GetPlayerData();
+        vectorLevelText = vectorLevel.GetComponent<UnityEngine.UI.Text>();
+        dragonLevelText = dragonLevel.GetComponent<UnityEngine.UI.Text>();
+        tempestLevelText = tempestLevel.GetComponent<UnityEngine.UI.Text>();
+        meteorLevelText = meteorLevel.GetComponent<UnityEngine.UI.Text>();
+        weapons = playerdata.GetWeapons();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Vector energy is: " + playerdata.GetWeapons()[0].CurrentLevelEnergy());
+        vectorMeter.SetValueMax(weapons[0].CurrentLevelEnergyMax());
+        vectorMeter.SetValue(weapons[0].CurrentLevelEnergy());
+        vectorLevelText.text = "Lv " + weapons[0].GetLevel().ToString();
+
+        dragonMeter.SetValueMax(weapons[1].CurrentLevelEnergyMax());
+        dragonMeter.SetValue(weapons[1].CurrentLevelEnergy());
+        dragonLevelText.text = "Lv " + weapons[1].GetLevel().ToString();
+
+        tempestMeter.SetValueMax(weapons[3].CurrentLevelEnergyMax());
+        tempestMeter.SetValue(weapons[3].CurrentLevelEnergy());
+        tempestLevelText.text = "Lv " + weapons[3].GetLevel().ToString();
+
+        meteorMeter.SetValueMax(weapons[2].CurrentLevelEnergyMax());
+        meteorMeter.SetValue(weapons[2].CurrentLevelEnergy());
+        meteorLevelText.text = "Lv " + weapons[2].GetLevel().ToString();
+    }
+}
